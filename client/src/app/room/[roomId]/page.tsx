db5fc7cac0c9191ff7676375d0c02315b75fdaa1
Spec-DY <ibonehp@gmail.com>
2025-05-04 00:43:07 -0700

    add roomid page

--- client/src/app/room/[roomId]/page.tsx
+++ client/src/app/room/[roomId]/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import React from "react";
+import { useUser } from "@/context/UserContext";
+import { useParams } from "next/navigation";
 
 export default function page() {
-  return <div>page</div>;
+  const { username, setUsername } = useUser();
+
+  const params = useParams();
+  const RoomId = params.roomId;
+  console.log("params is " + params);
+  console.log("Room ID is " + RoomId);
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      You logged in as {username} room id is {RoomId}
+    </div>
+  );
 }
